ffedd03ace688893dc673252adc52d37fac6b404
sandracr98 <92158166+sandracr98@users.noreply.github.com>
2025-10-30 10:41:13 +0100

    chore: initial Spring Boot project structure

---
+++ src/main/java/com/example/robot/RobotLimpiezaApplication.java
@@ -0,0 +1,13 @@
+package com.example.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotLimpiezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotLimpiezaApplication.class, args);
+	}
+
+}
